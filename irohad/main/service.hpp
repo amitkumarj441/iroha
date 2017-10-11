179c4acd334774d9132b30e0c7d6a02f59571c70
Bogdan Vaneev <warchantua@gmail.com>
2017-10-11 17:05:46 +0300

    Rename Service -> Application
    Signed-off-by: Bogdan Vaneev <warchantua@gmail.com>

--- irohad/main/service.hpp
+++ irohad/main/service.hpp
@@ -49,9 +49,9 @@
 
 using iroha::config::Config;
 
-class Service {
+class Application {
  public:
-  explicit Service(std::unique_ptr<Config>);
+  explicit Application(std::unique_ptr<Config>);
 
   const Config &config() const;
 
@@ -65,7 +65,7 @@ class Service {
    */
   virtual void run();
 
-  ~Service();
+  ~Application();
 
  protected:
   const std::unique_ptr<Config> config_;
